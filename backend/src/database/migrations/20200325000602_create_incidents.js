2d0d2ccdf535bb13a7dc211311edf5489a8815eb
Iury <iury@rarolabs.com.br>
2020-03-25 00:50:54 -0300

    migration corretion

--- backend/src/database/migrations/20200325000602_create_incidents.js
+++ backend/src/database/migrations/20200325000602_create_incidents.js
@@ -1,16 +1,16 @@
 exports.up = function(knex) {
-  knex.schema.createTable('incidents', function(table) {
+  return knex.schema.createTable('incidents', function(table) {
     table.increments();
 
-    table.string('title').primary();
+    table.string('title').notNullable();
     table.string('description').notNullable();
     table.decimal('value').notNullable();
 
     table.string('ong_id').notNullable();
-    table.foreignKey('ong_id').references('id').inTable('ongs');
+    table.foreign('ong_id').references('id').inTable('ongs');
   });
 };
 
 exports.down = function(knex) {
-  knex.schema.dropTable('incidents');
+  return knex.schema.dropTable('incidents');
 };
