aa6bdb04d5cebe522b3cbde9c574586d39acb28d
Iury <iury@rarolabs.com.br>
2020-03-25 00:34:27 -0300

    database creation

---
+++ backend/src/database/migrations/20200325000602_create_incidents.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  knex.schema.createTable('incidents', function(table) {
+    table.increments();
+
+    table.string('title').primary();
+    table.string('description').notNullable();
+    table.decimal('value').notNullable();
+
+    table.string('ong_id').notNullable();
+    table.foreignKey('ong_id').references('id').inTable('ongs');
+  });
+};
+
+exports.down = function(knex) {
+  knex.schema.dropTable('incidents');
+};
