41d161cb44125a7d5d6ea3d32731b53d0ab05041
iuryoliveira <iuryoliveirasx@gmail.com>
2020-03-28 20:15:08 -0300

    profile controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
 const SessionController = require("./controllers/SessionController");
+const ProfileController = require("./controllers/ProfileController");
 
 const routes = express.Router();
 
@@ -10,6 +11,8 @@ routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/profile", ProfileController.index);
 
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
