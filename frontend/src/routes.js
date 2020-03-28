2699713e679ea155ad21fc6c82bd6d17e2d30758
iuryoliveira <iuryoliveirasx@gmail.com>
2020-03-27 21:04:41 -0300

    profile header

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Logon from './pages/Logon';
-import Register from './pages/Register';
+import Logon from "./pages/Logon";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 export default function Routes() {
-  return(
+  return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
