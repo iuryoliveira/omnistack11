7d320fbabbb739a9233a1f563549fc5c159dedfa
iuryoliveira <iuryoliveirasx@gmail.com>
2020-03-29 15:42:33 -0300

    detail page elements

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,6 +1,52 @@
 import React from "react";
-import { View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { View, Image, TouchableOpacity, Text } from "react-native";
+
+import LogoImg from "../../assets/logo.png";
+
+import styles from "./styles";
 
 export default function Detail() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={LogoImg} />
+        <TouchableOpacity>
+          <Feather
+            name="arrow-left"
+            size={28}
+            color="#E02041"
+            onPress={() => {}}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={styles.incidentProperty}>ONG:</Text>
+        <Text style={styles.incidentValue}>APAD</Text>
+
+        <Text style={styles.incidentProperty}>Caso:</Text>
+        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+        <Text style={styles.incidentProperty}>Valor:</Text>
+        <Text style={styles.incidentValue}>R$ 120,00</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+        <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>Whatsapp</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 }
