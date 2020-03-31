a0667076fa42f5a497642b3608c3ad5319fbc406
iuryoliveira <iuryoliveirasx@gmail.com>
2020-03-31 00:13:35 -0300

    list incident test

--- backend/tests/integration/incidents.spec.js
+++ backend/tests/integration/incidents.spec.js
@@ -38,4 +38,49 @@ describe("Incidents", () => {
     expect(response.status).toEqual(400);
     expect(response.body.message).toEqual('"authorization" is required');
   });
+
+  it("Should be able to list incidents", async () => {
+    const ongName = "GBUD",
+      ongEmail = "teste@email.com",
+      ongWhatsapp = "3111111111",
+      ongCity = "Belo Horizonte",
+      ongUf = "MG";
+
+    const incidentTitle = "New incident",
+      incidentDescription = "Incident description",
+      incidentValue = 100.99;
+
+    const ongResponse = await request(app)
+      .post("/ongs")
+      .send({
+        name: ongName,
+        email: ongEmail,
+        whatsapp: ongWhatsapp,
+        city: ongCity,
+        uf: ongUf
+      });
+
+    const ong_id = ongResponse.body.id;
+
+    await request(app)
+      .post("/incidents")
+      .send({
+        title: incidentTitle,
+        description: incidentDescription,
+        value: incidentValue
+      })
+      .set("authorization", ong_id);
+
+    const response = await request(app).get("/incidents");
+    expect(response.body[0].id).toEqual(1);
+    expect(response.body[0].title).toEqual(incidentTitle);
+    expect(response.body[0].description).toEqual(incidentDescription);
+    expect(response.body[0].value).toEqual(incidentValue);
+    expect(response.body[0].ong_id).toEqual(ong_id);
+    expect(response.body[0].name).toEqual(ongName);
+    expect(response.body[0].email).toEqual(ongEmail);
+    expect(response.body[0].whatsapp).toEqual(ongWhatsapp);
+    expect(response.body[0].city).toEqual(ongCity);
+    expect(response.body[0].uf).toEqual(ongUf);
+  });
 });
